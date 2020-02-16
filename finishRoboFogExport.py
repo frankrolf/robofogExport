8992890503312fd70072ebbd03218d708e84e7ab
Frank Grießhammer <frankrolf@gmail.com>
2020-02-16 19:29:48 +0100

    Make the script work in RF3/Python3

--- finishRoboFogExport.py
+++ finishRoboFogExport.py
@@ -7,6 +7,7 @@
 
 """
 
+
 def cleanupBackground(f):
     moved = []
     for g in f:
@@ -14,17 +15,16 @@ def cleanupBackground(f):
             # do we have a foreground companion?
             other = g.name.replace("#", "")
             if other not in f:
-                print "background glyph %s has no foreground"%g.name
+                print("background glyph %s has no foreground" % g.name)
                 continue
             bg = f[other].getLayer("background")
             bg.clear()
             moved.append(g.name)
-            print bg
+            print(bg)
             bg.appendGlyph(g)
     for name in moved:
         f.removeGlyph(name)
 
 f = CurrentFont()
 cleanupBackground(f)
-print 'done'
-    
+print('done')
